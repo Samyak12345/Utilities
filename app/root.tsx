995bc2f77aba7c35a97ce4f99fd0e447a4d5f187
Samyak <jsamyak100@gmail.com>
2025-01-12 11:58:47 +0530

    updated meta tag for ads

--- app/root.tsx
+++ app/root.tsx
@@ -72,7 +72,7 @@ export default function App() {
 <GoogleAnalyticsScript/>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="google-adsense-account" content="ca-pub-1635371757353656">
+        <meta name="google-adsense-account" content="ca-pub-1635371757353656" />
         <Meta />
         <Links />
       </head>
